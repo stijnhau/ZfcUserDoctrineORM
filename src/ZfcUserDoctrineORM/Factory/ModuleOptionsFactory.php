2a17a1ab08361970d8c8e31cb4e80b6162711f60
Stijn Haulotte <stijnhau@gmail.com>
2017-01-24 21:20:25 +0100

    BC witH zf-2

--- src/ZfcUserDoctrineORM/Factory/ModuleOptionsFactory.php
+++ src/ZfcUserDoctrineORM/Factory/ModuleOptionsFactory.php
@@ -4,6 +4,7 @@ namespace ZfcUserDoctrineORM\Factory;
 use Zend\ServiceManager\FactoryInterface;
 use Interop\Container\ContainerInterface;
 use ZfcUserDoctrineORM\Options\ModuleOptions;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class ModuleOptionsFactory implements FactoryInterface
 {
@@ -15,5 +16,18 @@ class ModuleOptionsFactory implements FactoryInterface
         $config = $serviceLocator->get('Config');
 
         return new ModuleOptions(isset($config['zfcuser']) ? $config['zfcuser'] : array());
+    }
+
+    /**
+     * @deprecated ZF2 compability.
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        /* @var ServiceLocatorInterface $serviceLocator */
+        $serviceLocator = $serviceLocator->getServiceLocator();
+
+        $this->__invoke($serviceLocator, "ModuleOptionsFactory");
     }
 }
